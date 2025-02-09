d65cef5332eb4b3a18661c24d66c601ea1bd9d6b
OBAID <muhammadobaid103@gmail.com>
2025-02-10 00:12:35 +0500

    Add files via upload

---
+++ LAB_03/TASK_7.cpp
@@ -0,0 +1,9 @@
+/*Create a class for a stationary shop. The stationary shop maintains a list for all the items that it sells (hint: array of
+strings), and another list with the prices of the items (hint: array of prices).
+Create a menu-driven program to:
+1. Allow the shop owner to add the items and their prices.
+2. Fetch the list of items
+3. Edit the prices of the items
+4. View all the items and their prices
+Create a receipt that the shopkeeper can share with their customers. The receipt can only be created after the
+shopkeeper inputs the items and their amounts bought by the customer.*/
