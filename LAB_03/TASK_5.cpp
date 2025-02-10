dcdcd3b6e274c3386ec0697d9f77e22ee0c25e56
OBAID <muhammadobaid103@gmail.com>
2025-02-10 12:04:36 +0500

    Update TASK_5.cpp

--- LAB_03/TASK_5.cpp
+++ LAB_03/TASK_5.cpp
@@ -8,3 +8,58 @@ Create the following methods for your class:
  Show tasks. This method should go through all of your months and print all the tasks allocated.
 Your task is to create one calendar object, then hardcode 5-6 tasks for your calendar. Then demonstrate how
 you’ll remove a task, and display the updated task list.*/
+#include <iostream>
+using namespace std;
+class calender{
+    private:
+        string month[12][31];
+        int year;
+    public:
+        calender(int y){
+            for(int i=0;i<12;i++){
+                for (int j=0;j<31;j++){
+                    month[i][j]="\0";
+                }
+            }
+            year=y;
+        }
+        void AddTask(int day,int mon,string task){
+            if(day<1||day>31||mon<1||mon>12){
+                cout<<"Invalid Input"<<endl;
+            }
+            else{
+                month[mon-1][day-1]=task;
+            }
+        }
+        void removeTask(int day,int mon,string task ){
+            if(day<1||day>31||mon<1||mon>12){
+                cout<<"Invalid Input"<<endl;
+            }
+            else{
+                month[mon-1][day-1]="\0";
+            }
+        }
+        void display(){
+            cout<<"EVENTS FOR THE YEAR  "<<year<<endl;
+            for(int i=0;i<12;i++){
+                for (int j=0;j<31;j++){
+                    if(!month[i][j].empty()){
+                    cout<<"month    "<<i+1<<"   Day    "<<j+1<<"   "<<month[i][j]<<endl;
+                }}
+            }
+        }
+};
+int main(){
+    cout<<"ACTUAL EVENTS FOR THE YEAR"<<endl;
+    calender c1(2025);
+    c1.AddTask(25,1,"Birtday");
+    c1.AddTask(20,5,"EID");
+    c1.AddTask(10,6,"FEE SUBMISSSION");
+    c1.AddTask(9,9,"Assignment Submission");
+    c1.AddTask(25,12,"PAKISTAN DAY");
+    c1.display();
+    cout<<" REMOVING TASK(ASSIGNMENT SUBMISSION)"<<endl;
+    cout<<" EVENTS AFTER REMOVAL"<<endl;
+    c1.removeTask(9,9,"Assignment Submission");
+    c1.display();
+}
