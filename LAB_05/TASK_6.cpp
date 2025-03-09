fa81e68ff9812f0349b9d475de6c704bea3c1810
OBAID <muhammadobaid103@gmail.com>
2025-03-10 00:26:10 +0500

    Create TASK_6.cpp

---
+++ LAB_05/TASK_6.cpp
@@ -0,0 +1,6 @@
+/*Write a program that uses composition to implement a game engine. A game engine is madeup of
+several components. For example:
+a) Graphics rendering engine
+b) Input handler
+c) Physics engine
+You don’t have to write the logicforhow these individual components work.*/
