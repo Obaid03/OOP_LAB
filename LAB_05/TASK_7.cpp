e1aa7252501b301150fc642e9c8d9bd5e5d84e7c
OBAID <muhammadobaid103@gmail.com>
2025-03-10 00:26:36 +0500

    Create TASK_7.cpp

---
+++ LAB_05/TASK_7.cpp
@@ -0,0 +1,11 @@
+/*Implement a restaurant ordering system that holds information about the restaurant’s menus, menu
+items, orders, and payments. Identify the relationship between the five entities mentioned. Keep in
+mind the following information as well:
+a) Menu Items hold two things: food name and food price.
+b) Menu is a class that holds an array of menu items. You can have different functions to add and
+remove items, or display the entire menu.
+c) Restaurant ordering system has one menu.
+d) Any staff member can place an order to the system on behalf of a customer. Theorder class
+consists of one or more menu items and a payment.
+e) Payment is a class that holds the amount of money that a customer needs to pay.This is
+generated when the order is placed.*/
