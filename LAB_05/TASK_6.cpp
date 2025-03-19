b2c3b3d37c53f65ccf56dabc6aa1c797a8a847be
OBAID <muhammadobaid103@gmail.com>
2025-03-20 00:29:03 +0500

    Update TASK_6.cpp

--- LAB_05/TASK_6.cpp
+++ LAB_05/TASK_6.cpp
@@ -4,3 +4,91 @@ a) Graphics rendering engine
 b) Input handler
 c) Physics engine
 You don’t have to write the logicforhow these individual components work.*/
+
+
+#include <iostream>
+using namespace std;
+class Graphicsrenderengine{
+public:
+    Graphicsrenderengine()
+    {
+        cout << "Graphics Render Engine has been created." << endl;
+    }
+
+    ~Graphicsrenderengine()
+    {
+        cout << "Graphics Render Engine has been destroyed." << endl;
+    }
+    void render()
+    {
+        cout << "Rendering game objects..." << endl;
+    }
+};
+
+class InputHandler
+{
+public:
+    InputHandler()
+    {
+        cout << "Input Handler has been created." << endl;
+    }
+
+    ~InputHandler()
+    {
+        cout << "Input Handler has been destroyed." << endl;
+    }
+    void processInput()
+    {
+        cout << "Processing user input..." << endl;
+    }
+};
+
+class PhysicsEngine
+{
+public:
+    PhysicsEngine()
+    {
+        cout << "Physics Engine has been created." << endl;
+    }
+
+    ~PhysicsEngine()
+    {
+        cout << "Physics Engine has been destroyed." << endl;
+    }
+    void updatePhysics()
+    {
+        cout << "Updating physics simulation..." << endl;
+    }
+};
+
+class GameEngine
+{
+    Graphicsrenderengine graphics;
+    InputHandler input;
+    PhysicsEngine physics;
+
+public:
+    GameEngine()
+    {
+        cout << "Game Engine has been created." << endl;
+    }
+
+    ~GameEngine()
+    {
+        cout << "Game Engine has been destroyed." << endl;
+    }
+    void run()
+    {
+        cout << "Game Loop Running..." << endl;
+        input.processInput();
+        physics.updatePhysics();
+        graphics.render();
+    }
+};
+
+int main()
+{
+    GameEngine game;
+    game.run();
+    return 0;
+}
