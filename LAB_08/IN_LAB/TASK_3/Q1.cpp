a184ea4647405bb86a703df160a38da30554bed1
OBAID <muhammadobaid103@gmail.com>
2025-04-12 22:36:35 +0500

    Update Q1.cpp

--- LAB_08/IN_LAB/TASK_3/Q1.cpp
+++ LAB_08/IN_LAB/TASK_3/Q1.cpp
@@ -9,7 +9,7 @@ Justify your answer with supporting C++ code.*/
 
 
 //Answer
-/*Yes, a friend function can be used to overload an operator*/
+/*YES*/
 
 //Implementation
 
@@ -17,28 +17,22 @@ Justify your answer with supporting C++ code.*/
 using namespace std;
 class complex{
     private:
-        int a,b;
+        int a;
     public:
-        complex(int x =0, int y=0):a(x),b(y){}
-        friend complex operator +=(complex &c,complex &c1);
+        complex(int x =0):a(x){}
+        friend complex operator +=(complex &c,const complex &c1);
         void show(){
             cout <<"A =     "<<a<<endl;
-            cout <<"B =     "<<b<<endl;
         }
 };
-complex operator+=(complex &c,complex &c1){
-    complex temp;
-    temp.a=c.a+c1.a;
-    temp.b=c.b+c1.b;
-    // temp.a+=c.a;
-    // temp.b+=c.b;
-    return temp;
+complex operator+=(complex &c,const complex &c1){
+    c.a+=c1.a;
+    return c;
 }
 int main(){
-    complex c(4,4),c1(4,4),c2;
+    complex c(4),c1(4);
     c.show();
     c1.show();
-    c2=operator+=(c,c1);
-    c2.show();
-
+    c+=c1;
+    c.show();
 }
