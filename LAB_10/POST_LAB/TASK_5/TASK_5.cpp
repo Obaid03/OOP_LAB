aea8fa2e019fb64e9248f0b89d539978285f48aa
Obaid03 <muhammadobaid103@gmail.com>
2025-04-27 14:06:37 +0500

    Task_5 Updated

---
+++ LAB_10/POST_LAB/TASK_5/TASK_5.cpp
@@ -0,0 +1,65 @@
+/*1. Create and populate a file named data.txt with a few sentences. (e.g., AI is the
+future. AI will transform the world. Embrace AI now.)
+2. Prompt the user to enter:
+○ A searchWord (e.g., "AI")
+○ A replacementWord (e.g., "ML")
+3. Open the file using fstream in ios::in | ios::out mode.
+4. Search for the searchWord using seekg() and read sequentially.
+5. When found:
+○ Use tellg() to get the current read pointer location.
+○ Use seekp() to position the write pointer.
+○ Replace the word only if the replacement is of equal or smaller length to
+avoid shifting content.
+
+6. If the word is replaced, pad with spaces if needed (e.g., replacing "AI" with "ML" or
+"XX").
+7. After all replacements, display the updated file.*/
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main(){
+    string search,replace,word,line;
+    ofstream file("data.txt",ios::out);
+    if(!file){
+        cout<<"Error Opening Data.txt"<<endl;
+        return 1;
+    }
+    file<<"AI is the future. AI will transform the world. Embrace AI now."<<endl;
+    file.close();
+    cout<<"Enter a Word to Search"<<endl;
+    cin>>search;
+    cout<<"Enter a Word to Replace"<<endl;
+    cin>>replace;
+    if(!(search.size()==replace.size())){
+        cout<<"Invalid Input! Both words should have same length"<<endl;
+    }
+    fstream File("data.txt",ios::in|ios::out);
+    if(File){
+    try{
+        while(File>>word){
+            // cout<<File.tellg()<<endl;
+            if(word==search){
+                int n=File.tellg();
+                n-=word.size();
+                throw n-1;//bcz reading word by word.After reading any word it adds space also 
+            }
+        }
+        cout<<"Word Does not Exists"<<endl;
+    }
+    catch(int pos){
+        File.seekp(pos);
+        File<<replace;
+        cout<<word<<"   Replaced with   "<<replace<<endl;
+    }
+    File.clear();
+    File.seekg(0);
+    getline(File,line);
+    cout<<line<<endl;
+    File.close();
+}
+else{
+    cout << "Error Opening Data.txt" << endl;
+        return 1;
+}
+return 0;
+}
