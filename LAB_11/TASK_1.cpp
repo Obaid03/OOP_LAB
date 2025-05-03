f388771dbc89b1cb4ce2b537d5b89df5d3b92650
Obaid03 <muhammadobaid103@gmail.com>
2025-05-03 19:53:33 +0500

    TASK_1 Updated

--- LAB_11/TASK_1.cpp
+++ LAB_11/TASK_1.cpp
@@ -1,5 +1,27 @@
 #include<iostream>
+#include <exception>
 using namespace std;
+class InvalidValueException:public exception{
+    public:
+        const char* what()const noexcept override{
+            return "Age Cannot be nagative or greater than 120";
+        }
+};
+void validateAge(int age){
+    if (age>120||age<0){
+        throw InvalidValueException();
+    }
+    cout<<"Age Successfuly Entered"<<endl;
+}
 int main(){
-    cout<<"Hello World"<<endl;
+        int age;
+    try{
+        cout<<"Enter Your age"<<endl;
+        cin>>age;
+        validateAge(age);
+    }
+    catch(InvalidValueException I){
+        cout<<I.what();
+    }
+    
 }
