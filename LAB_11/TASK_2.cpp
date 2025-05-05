3c833010db80dad4b42d857ab78e3e6afc97863f
Obaid03 <muhammadobaid103@gmail.com>
2025-05-05 23:20:32 +0500

    Update

--- LAB_11/TASK_2.cpp
+++ LAB_11/TASK_2.cpp
@@ -1,5 +1,64 @@
 #include<iostream>
+#include<exception>
 using namespace std;
+class StackOverflowException:public exception{
+    public:
+        const char* what()const noexcept override{
+            return "Stack is Full";
+        }
+};
+class StackUnderflowException:public exception{
+    public:
+        const char* what()const noexcept override{
+            return "Stack is Empty";
+        }
+};
+template <typename T> class Stack{
+    private:
+       static const int cap=2;
+        T capacity[cap];
+        int size;
+    public:
+        Stack(){
+            capacity[0]=T();
+            capacity[1]=T();
+            size=-1;
+        }
+        void push(int n){
+            try{
+                if(size>=cap-1){
+                    throw StackOverflowException();
+                }
+                else {
+                    capacity[++size]=n;
+                }
+            }
+            catch(StackOverflowException& oe){
+                cout<<oe.what()<<endl;
+            }
+            
+        }
+        void pop(){
+            try{
+                if(size<0){
+                    throw StackUnderflowException();
+                }
+                else {
+                    capacity[size--];
+                }
+            }
+            catch(StackUnderflowException& e)
+            {
+                std::cerr << e.what() << '\n';
+            }
+            
+            
+        }
+};
 int main(){
-    cout<<"Hello World"<<endl;
+    Stack<int> s1,s2;
+    s1.push(10);
+    s1.push(20);
+    s1.push(30);
+    s2.pop();
 }
